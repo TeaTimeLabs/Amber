d388e002bd58507aea1206187cd74a414abd7084
moneysignsteben <moneysignsteben@gmail.com>
2021-06-23 15:31:56 -0700

    Removed End Pirates

--- src/main/java/me/nykorrin/amber/listeners/EntityListener.java
+++ src/main/java/me/nykorrin/amber/listeners/EntityListener.java
@@ -41,6 +41,12 @@ public class EntityListener implements Listener {
                 this.plugin.getLogger().info("Kokushibo entity detected. UUID of NPC is " + entity.getUniqueId());
             }
         }
+
+        if (!SucroseAPI.getManagerHandler().getEventManager().getDoma().getNPCs().isEmpty()) {
+            if (SucroseAPI.getManagerHandler().getEventManager().getDoma().getNPCs().contains(npc)) {
+                this.plugin.getLogger().info("Doma entity detected. UUID of NPC is " + entity.getUniqueId());
+            }
+        }
     }
 
     @EventHandler (priority = EventPriority.LOWEST)
@@ -53,13 +59,31 @@ public class EntityListener implements Listener {
             return;
         }
 
-        if (SucroseAPI.getManagerHandler().getEventManager().getKokushibo().getNPCs().contains(npc)) { //
+        if (SucroseAPI.getManagerHandler().getEventManager().getKokushibo().getNPCs().contains(npc)) {
             this.plugin.getLogger().info("Kokushibo entity killed. UUID of Kokushibo was " + entity.getUniqueId());
 
             double amount = this.plugin.getConfig().getDouble("events.kokushibo.kokushibo");
 
             Amber.getEconomy().depositPlayer(player, amount).transactionSuccess();
             Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + player.getName() + " earned $" + amount + " for killing Upper Moon One: Kokushibō.");
+        }
+
+        if (SucroseAPI.getManagerHandler().getEventManager().getDoma().getNPCs().contains(npc)) {
+            this.plugin.getLogger().info("Doma entity killed. UUID of Doma was " + entity.getUniqueId());
+
+            double amount = this.plugin.getConfig().getDouble("events.doma.doma");
+
+            Amber.getEconomy().depositPlayer(player, amount).transactionSuccess();
+            Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + player.getName() + " earned $" + amount + " for killing Upper Moon Two: Dōma.");
+        }
+
+        if (SucroseAPI.getManagerHandler().getEventManager().getDoma().getChildren().contains(npc)) {
+            this.plugin.getLogger().info("Doma entity killed. UUID of Crystalline Divine Child was " + entity.getUniqueId());
+
+            double amount = this.plugin.getConfig().getDouble("events.doma.crystalline_divine_child");
+
+            Amber.getEconomy().depositPlayer(player, amount).transactionSuccess();
+            Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + player.getName() + " earned $" + amount + " for killing a Crystalline Divine Child.");
         }
     }
 
@@ -74,10 +98,6 @@ public class EntityListener implements Listener {
 
             if (SucroseAPI.getManagerHandler().getEventManager().getNetherRaid().getEntites().contains(entity.getCustomName())) {
                 this.plugin.getLogger().info("Nether Raid entity detected. UUID of " + entity.getType().name() + " is " + entity.getUniqueId());
-            }
-
-            if (SucroseAPI.getManagerHandler().getEventManager().getEndPirates().getEntites().contains(entity.getCustomName())) {
-                this.plugin.getLogger().info("End Pirate entity detected. UUID of " + entity.getType().name() + " is " + entity.getUniqueId());
             }
         }
 
@@ -151,26 +171,6 @@ public class EntityListener implements Listener {
 
                 if (entity.getCustomName().contains("Zombie Piglin Minion")) {
                     amount = this.plugin.getConfig().getDouble("events.nether_raid.zombie_piglin_minion");
-                }
-
-                Amber.getEconomy().depositPlayer(player, amount).transactionSuccess();
-                Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + player.getName() + " earned $" + amount + " for killing a " + entity.getName() + ChatColor.LIGHT_PURPLE + ".");
-                return;
-            }
-
-            if (SucroseAPI.getManagerHandler().getEventManager().getEndPirates().getEntites().contains(entity.getCustomName())) {
-                this.plugin.getLogger().info("End Pirates entity killed. UUID of " + entity.getName() + " was " + entity.getUniqueId());
-
-                if (entity.getCustomName().contains("Enderman Pirate Captain")) {
-                    amount = this.plugin.getConfig().getDouble("events.end_pirates.enderman_pirate_captain");
-                }
-
-                if (entity.getCustomName().contains("Enderman Pirate Gunner")) {
-                    amount = this.plugin.getConfig().getDouble("events.end_pirates.enderman_pirate_gunner");
-                }
-
-                if (entity.getCustomName().contains("Enderman Pirate")) {
-                    amount = this.plugin.getConfig().getDouble("events.end_pirates.enderman_pirate");
                 }
 
                 Amber.getEconomy().depositPlayer(player, amount).transactionSuccess();
